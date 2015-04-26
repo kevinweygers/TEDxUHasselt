cb3d78bcb7939d6101476b26c08933b3b1c23ed9
laurenspouders <laurens.pouders@student.pxl.be>
2015-04-26 15:37:23 +0200

    poging1

--- README.md
+++
@@ -1,2 +0,0 @@
-# TEDxUHasselt
-project webtech
--- main/application/views/view_about.php
+++
@@ -1,9 +0,0 @@
-
-        <div class="container">
-            About TEDxUHasselt
-
-            TEDxUHasselt is an independently organized TED conference where speakers from around the world share cutting-edge ideas. During the breaks, attendees connect with each other through various interactive activities.
-
-            TEDxUHasselt was founded in 2011 by three Hasselt University students and has remained a student-driven organization ever since. Meet our team members!  
-        </div>
- 
